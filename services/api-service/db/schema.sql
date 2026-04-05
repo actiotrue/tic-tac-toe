d84647d5d1219cb3a20821c26826d41a24dfb9bf
Jud1k <astronsahsa@mail.ru>
2026-04-05 21:39:27 +0500

    fixes

--- services/api-service/db/schema.sql
+++ services/api-service/db/schema.sql
@@ -1,7 +1,7 @@
 \restrict dbmate
 
--- Dumped from database version 18.1 (Ubuntu 18.1-1.pgdg24.04+2)
--- Dumped by pg_dump version 18.1 (Ubuntu 18.1-1.pgdg24.04+2)
+-- Dumped from database version 18.3 (Ubuntu 18.3-1.pgdg24.04+1)
+-- Dumped by pg_dump version 18.3 (Ubuntu 18.3-1.pgdg24.04+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -21,7 +21,7 @@ SET row_security = off;
 
 CREATE TYPE public.game_result AS ENUM (
     'x_won',
-    'y_won',
+    'o_won',
     'draw'
 );
 
