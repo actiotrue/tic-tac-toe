b4e3cc625f10c6faa935f02f2a246183e1129f58
Jud1k <astronsahsa@mail.ru>
2026-04-06 17:29:07 +0500

    add cors origins

--- services/api-service/app/core/settings.py
+++ services/api-service/app/core/settings.py
@@ -1,5 +1,15 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Any
 
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing_extensions import Annotated
+from pydantic import AnyUrl, BeforeValidator, computed_field
+
+def parse_cors(v: Any) -> list[str] | str:
+    if isinstance(v, str) and not v.startswith("["):
+        return [i.strip() for i in v.split(",") if i.strip()]
+    elif isinstance(v, list | str):
+        return v
+    raise ValueError(v)
 
 class Settings(BaseSettings):
     DB_HOST: str = "localhost"
@@ -25,6 +35,16 @@ class Settings(BaseSettings):
     CLOUDINARY_BASE_IMAGE_PUBLIC_ID: str
 
     INTERNAL_SERVICE_KEY: str
+
+
+    BACKEND_CORS_ORIGINS: Annotated[
+        list[AnyUrl] | str, BeforeValidator(parse_cors)
+    ] = []
+
+    @computed_field
+    @property
+    def all_cors_origins(self) -> list[str]:
+        return [str(origin).rstrip("/") for origin in self.BACKEND_CORS_ORIGINS]
 
     model_config = SettingsConfigDict(
         env_ignore_empty=True, extra="allow", env_file="../.env"
